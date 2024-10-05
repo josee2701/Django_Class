b15a27efc6c1b2e5fbb371ad5d794f39ddae1812
Jose Campos <j.camposs2701@gmail.com>
2024-10-04 22:03:07 -0500

    feat: Se realiza toda la parte de la configuracion inicial del proyecto donde se crea los modelos y las relaciones

---
+++ supermercado/admin.py
@@ -0,0 +1,71 @@
+# Register your models here.
+from django.contrib import admin
+
+from .models import Cliente, MetodoPago, Product, ProductoVenta, Stock, Venta
+
+
+class ProductoVentaInline(admin.TabularInline):
+    """
+    Permite añadir productos a la venta desde la vista de Venta.
+    """
+    model = ProductoVenta
+    extra = 1  # Número de formularios adicionales para añadir nuevos productos en la vista de ventas
+
+
+class VentaAdmin(admin.ModelAdmin):
+    """
+    Personaliza el modelo de Venta en el admin.
+    """
+    list_display = ('id', 'cliente', 'fecha', 'total', 'metodo_pago')
+    inlines = [ProductoVentaInline]  # Permite añadir productos dentro de la venta
+    readonly_fields = ('total',)
+
+    def total(self, obj):
+        """
+        Calcula el total de la venta para mostrarlo en la vista del admin.
+        """
+        return obj.total
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Personaliza la visualización del modelo Product.
+    """
+    list_display = ('nombre', 'precio', 'color')
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    """
+    Personaliza la visualización del modelo Cliente.
+    """
+    list_display = ('nombre', 'telefono', 'correo', 'direccion')
+
+
+class StockAdmin(admin.ModelAdmin):
+    """
+    Personaliza la visualización del modelo Stock.
+    """
+    list_display = ('producto', 'cantidad', 'fecha')
+
+
+class ProductoVentaAdmin(admin.ModelAdmin):
+    """
+    Personaliza la visualización del modelo ProductoVenta.
+    """
+    list_display = ('venta', 'producto', 'cantidad', 'precio_unitario')
+
+
+class MetodoPagoAdmin(admin.ModelAdmin):
+    """
+    Personaliza la visualización del modelo MetodoPago.
+    """
+    list_display = ('tipo',)
+
+
+# Registro de los modelos en el panel de administración
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Stock, StockAdmin)
+admin.site.register(MetodoPago, MetodoPagoAdmin)
+admin.site.register(Venta, VentaAdmin)
+admin.site.register(ProductoVenta, ProductoVentaAdmin)
---
+++ supermercado/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=255)
+    direccion = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=20)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Product(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)  # Usar 2 decimales para el precio
+    imagen = models.FileField(upload_to='productos/')
+    color = models.TextField()
+
+    def __str__(self):
+        return f"{self.nombre}"
+
+
+class Stock(models.Model):
+    producto = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.producto} - {self.cantidad} - {self.fecha}'
+
+
+class MetodoPago(models.Model):
+    tipo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tipo
+
+
+class Venta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Venta {self.id} - {self.fecha} - {self.cliente}"
+
+    @property
+    def total(self):
+        """
+        Calcula el total de la venta sumando los precios de los productos de ProductoVenta.
+        """
+        productos_venta = self.productos.all()
+        total = sum([item.precio_unitario * item.cantidad for item in productos_venta])
+        return total
+
+
+class ProductoVenta(models.Model):
+    venta = models.ForeignKey(Venta, related_name='productos', on_delete=models.CASCADE)
+    producto = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)  # Precio del producto en el momento de la venta
+
+    def __str__(self):
+        return f"{self.producto.nombre} - Cantidad: {self.cantidad}"
+
+    def save(self, *args, **kwargs):
+        """
+        Al guardar la venta, asegurarse de registrar el precio actual del producto en precio_unitario
+        """
+        if not self.precio_unitario:
+            self.precio_unitario = self.producto.precio
+        super().save(*args, **kwargs)
